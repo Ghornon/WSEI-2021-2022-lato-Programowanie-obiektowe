c7d7165856bd378b005b554e1508d4678c9a663f
Ghornon <szymon198198@gmail.com>
2022-04-27 09:40:36 +0200

    Add dir support

--- lab_4/lab_4/FileReader.cs
+++ lab_4/lab_4/FileReader.cs
@@ -5,25 +5,34 @@ using System.IO;
 
 namespace lab_4
 {
-    class File
+    enum FileTypes
     {
-        private string name, path, extension;
-        private long size;
+        dir,
+        image,
+        audio,
+        video,
+        document,
+        other
+    };
+    class Dir
+    {
+        protected string name, path;
+        protected long size;
+        protected FileTypes type;
         public string Name { get => name; }
         public string Path { get => path; }
         public long Size { get => size; }
         public string SizeWithSuffix { get => SizeSuffix(size); }
-        public string Extension { get => extension; }
-        public File(string path, long size)
+        public FileTypes Type { get => type; }
+        public Dir(string path, long size)
         {
             this.path = path;
             this.size = size;
             this.name = path.Split('\\').Last();
-            this.extension = this.name.Split('.').Last();
+            this.type = FileTypes.dir;
         }
 
-        static readonly string[] SizeSuffixes =
-                  { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
 
         static string SizeSuffix(long value)
         {
@@ -40,30 +49,50 @@ namespace lab_4
             return string.Format("{0:n1} {1}", dValue, SizeSuffixes[i]);
         }
     }
+    class File : Dir
+    {
+        private string extension;
+        public string Extension { get => extension; }
+        public File(string path, long size) : base (path, size)
+        {
+            this.extension = this.name.Split('.').Last();
+            this.type = GetFileType();
+        }
+
+        private FileTypes GetFileType()
+        {
+            string[] images = { "png", "webp", "jpg", "gif", "tiff" };
+            string[] audio = { "ogg", "mp2" };
+            string[] video = { "mkv", "mp4", "webm" };
+            string[] document = { "txt", "doc", "docx", "xml", "xlmx" };
+
+            if (images.Contains(this.extension))
+                return FileTypes.image;
+
+            if (audio.Contains(this.extension))
+                return FileTypes.image;
+
+            if (video.Contains(this.extension))
+                return FileTypes.image;
+
+            if (document.Contains(this.extension))
+                return FileTypes.image;
+
+            return FileTypes.other;
+        }
+    }
 
     class FileReader
     {
+        public List<Dir> DirsList = new List<Dir>();
         public List<File> FilesList = new List<File>();
-        public List<File> DirsList = new List<File>();
 
         public FileReader(string path = "C:\\Skany")
         {
             try
             {
-                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
-                var dirs = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
-
-                foreach (string file in files)
-                {
-                    long length = new FileInfo(file).Length;
-                    FilesList.Add(new File(file, length));
-                }
-
-                foreach (string dir in dirs)
-                {
-                    Console.WriteLine(dir);
-                }
-
+                FilesList = EnumerateFiles();
+                DirsList = EnumerateDirectories();
             }
             catch (UnauthorizedAccessException uAEx)
             {
@@ -79,7 +108,51 @@ namespace lab_4
             {
                 Console.WriteLine($"{file.Name} {file.Path} {file.SizeWithSuffix} {file.Extension}");
             }
+
+            foreach (var dir in DirsList)
+            {
+                Console.WriteLine($"{dir.Name} {dir.Path} {dir.SizeWithSuffix}");
+            }
         }
+
+        private List<File> EnumerateFiles(string path = "C:\\Skany")
+        {
+            List<File> list = new List<File>();
+
+            var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                long length = new FileInfo(file).Length;
+                list.Add(new File(file, length));
+            }
+
+            return list;
+        }
+
+        private List<Dir> EnumerateDirectories(string path = "C:\\Skany")
+        {
+            List<Dir> list = new List<Dir>();
+
+            var dirs = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+            
+
+            foreach (string dir in dirs)
+            {
+                long length = 0;
+                var files = EnumerateFiles(dir);
+
+                foreach (File file in files)
+                {
+                    length += file.Size;
+                }
+
+                list.Add(new Dir(dir, length));
+
+            }
+
+            return list;
+        }
+
     }
 
 
