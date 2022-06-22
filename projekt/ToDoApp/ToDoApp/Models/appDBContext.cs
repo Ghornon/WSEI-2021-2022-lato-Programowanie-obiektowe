44362d0239b696b5dc285c7a7d25ae699deb2221
Ghornon <szymon198198@gmail.com>
2022-06-22 16:57:01 +0200

    Init project

---
+++ projekt/ToDoApp/ToDoApp/Models/appDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApp.Models
+{
+    class appDBContext : DbContext
+    {
+
+    }
+}
