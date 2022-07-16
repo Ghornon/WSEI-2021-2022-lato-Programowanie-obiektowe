6c3ab4c30b420834cb647dd8a74ea92048188210
Ghornon <szymon198198@gmail.com>
2022-07-16 17:09:28 +0200

    Add tests

--- Szymon_Guzik_13659/TestProject1/UnitTest1.cs
+++ Szymon_Guzik_13659/TestProject1/UnitTest1.cs
@@ -16,6 +16,14 @@ namespace TestProject1
             Assert.AreEqual(a, a);
             Assert.AreEqual(a, b);
             Assert.AreNotEqual(c, a);
+
+            //TODO: zaproponuj asercje dla porównania c z b
+            Assert.AreNotEqual(c, b);
+            Assert.AreNotSame(c, b);
+
+            //TODO: zaproponuj asercje dla porównania b z c
+            Assert.AreNotEqual(b, c);
+            Assert.AreNotSame(b, c);
         }
     }
 }
